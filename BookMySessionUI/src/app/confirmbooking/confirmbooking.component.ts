5f9ff34245a6206c8e90d41a7e97323c6961c327
Rajesh Chaudhary <rchaudhary@ih-corp.com>
2019-03-14 18:26:35 +0530

    validations added in confirmbooking.component.html

--- BookMySessionUI/src/app/confirmbooking/confirmbooking.component.ts
+++ BookMySessionUI/src/app/confirmbooking/confirmbooking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-confirmbooking',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./confirmbooking.component.css']
 })
 export class ConfirmbookingComponent implements OnInit {
-
-  constructor() { }
+  registerForm: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]]
+      
+    });
   }
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+  onSubmit() {   
+    this.submitted = true;
 
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    // alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
+  }
 }
--- BookMySessionUI/src/app/register/register.component.ts
+++ BookMySessionUI/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,17 +7,33 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  form = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    mobile: new FormControl('', [Validators.required,Validators.minLength(10),Validators.pattern(/^-?(0|[1-9]\d*)?$/)]),
-    password: new FormControl('', [Validators.required,Validators.minLength(6)])
-  });
-  constructor() { }
+
+  registerForm: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({     
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)]],
+      password:['', [Validators.required,Validators.minLength(6)]]
+      
+    });
   }
   showPassword() {
     var inputPassword = document.querySelector('#input-password');
     inputPassword.setAttribute('type', 'text');
+  }
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+  onSubmit() {   
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
   }
 }
